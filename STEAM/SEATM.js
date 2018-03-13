3d58238828bae03f1219a18cbc75cdad223893ef
不就是我咯 <1491306368@qq.com>
2018-03-13 11:05:22 +0800

    a

--- STEAM/SEATM.js
+++ STEAM/SEATM.js
@@ -102,20 +102,20 @@ $(".next").click(function(){
 	}
 })
 // 自动播放逻辑函数
-function autoNext(){
-	$all_lbtList = $(".autoplay").eq(0).find(".all_lbt")
-	for (var i = 0; i < $all_lbtList.length; i++) {
-		if ($all_lbtList.eq(i).hasClass("focus")) {
-			if (i < $all_lbtList.length - 1) {
-				hideORfadeIn(i+1,$(".autoplay").eq(0).find(".next"))
-			}
-			else {
-				hideORfadeIn(0,$(".autoplay").eq(0).find(".next"))
-			}
-			break
-		}
-	}
-}
+// function autoNext(){
+// 	$all_lbtList = $(".autoplay").eq(0).find(".all_lbt")
+// 	for (var i = 0; i < $all_lbtList.length; i++) {
+// 		if ($all_lbtList.eq(i).hasClass("focus")) {
+// 			if (i < $all_lbtList.length - 1) {
+// 				hideORfadeIn(i+1,$(".autoplay").eq(0).find(".next"))
+// 			}
+// 			else {
+// 				hideORfadeIn(0,$(".autoplay").eq(0).find(".next"))
+// 			}
+// 			break
+// 		}
+// 	}
+// }
 // 小圆点点击事件
 $(".dot span").click(function(){
 	$all_lbtList = $(this).closest(".autoplay").find(".all_lbt")
@@ -128,17 +128,17 @@ $(".dot span").click(function(){
 	}
 })
 // // 自动播放逻辑
-var firstTime = setInterval(function(){
-	autoNext()
-},2000)
-$(".autoplay").eq(0).mouseenter(function(){
-	clearInterval(firstTime)
-})
-$(".autoplay").eq(0).mouseleave(function(){
-	firstTime = setInterval(function(){
-		autoNext()
-	},2000)
-})
+// var firstTime = setInterval(function(){
+// 	autoNext()
+// },2000)
+// $(".autoplay").eq(0).mouseenter(function(){
+// 	clearInterval(firstTime)
+// })
+// $(".autoplay").eq(0).mouseleave(function(){
+// 	firstTime = setInterval(function(){
+// 		autoNext()
+// 	},2000)
+// })
 // 第二个轮播图倒计时事件
 var $time = $("#hel_time")
 // 倒计时函数
@@ -211,6 +211,8 @@ $(".chose_list").mouseenter(function(){
 	}
 })
 // 详情页的轮播图逻辑
+var $dot_minList = $(this).closest(".dot_min").find("li")
+var count = $(".dot_min").find("li").length
 $(".dot_min li").click(function(){
 	$all_lbtList = $(this).closest(".left_lbt").find(".main_lbt_7")
 	$all_lbtList.hide()
@@ -235,11 +237,25 @@ function main_lbt_7(index,obj){
 	$all_lbtList.eq(index).fadeIn()
 	$dot_minList.eq(index).addClass("dot_min_bd")
 }
+var mhlocal = false
 $(".game_next").click(function(){
-	var ml = parseInt($(".ul")[0].style.marginLeft)
+	var ml = parseInt($("._ul")[0].style.marginLeft)
 	$all_lbtList = $(this).closest(".left_lbt").find(".main_lbt_7")
 	for (var i = 0; i < $all_lbtList.length; i++) {
 		if ($all_lbtList.eq(i).hasClass("focus")) {
+			if ((i+1) % 5 == 0) {
+				nt = i + 1 
+				if (!mhlocal) {
+					if (count - nt < 5) {
+						mhlocal = true
+						main_lbt_7(nt,this)
+						$("._ul").eq(0).animate({
+							"marginLeft":ml-(count - nt)*120
+						})
+						break
+					}
+				}
+			}	
 			if ((i+1)%5 !== 0 && i !== 0 && i !== $all_lbtList.length-1) {
 				main_lbt_7(i+1,this)
 				break
@@ -248,18 +264,23 @@ $(".game_next").click(function(){
 				main_lbt_7(i+1,this)
 				break
 			}
-			if (i !== 0 && (i+1)%5 == 0 && i !== $all_lbtList.length - 1) {
+			if (i !== 0 && (i+1)%5 == 0 && count - nt < 5) {
 				main_lbt_7(i+1,this)
-				$(".ul").eq(0).animate({
-					"marginLeft":-600
+				break
+			}		
+			if (i !== 0 && (i+1)%5 == 0 && i !== $all_lbtList.length - 1) {
+				$("._ul").eq(0).animate({
+					"marginLeft":-nt * 120
 				})
+				main_lbt_7(i+1,this)
 				break
 			}
 			else {
 				main_lbt_7(0,this)
-				$(".ul").eq(0).animate({
+				$("._ul").eq(0).animate({
 					"marginLeft":0
 				})
+				mhlocal = false
 				break
 			}
 		}	
@@ -267,38 +288,146 @@ $(".game_next").click(function(){
 })
 $(".game_prev").click(function(){
 	$all_lbtList = $(this).closest(".left_lbt").find(".main_lbt_7")
-	var ml = parseInt($(".ul")[0].style.marginLeft)
+	var ml = parseInt($("._ul")[0].style.marginLeft)
 	for (var i = 0; i < $all_lbtList.length; i++) {
 		if ($all_lbtList.eq(i).hasClass("focus")) {
-			if (i !== 0 && i%5 !== 0) {
+			if (i !== 0 && i + 1 == 5) {
 				main_lbt_7(i-1,this)
 				break
 			}
 			if (i == 0) {
 				main_lbt_7($all_lbtList.length - 1,this)
-				$(".ul").eq(0).animate({
-					"marginLeft":-600
+				$("._ul").eq(0).animate({
+					"marginLeft":-(count - 5) * 120
 				})
 				break
 			}
-			if (i !== 0 && i%5 == 0) {
+			else{
 				main_lbt_7(i-1,this)
-				$(".ul").eq(0).animate({
-					"marginLeft":ml + 600
-				})
 				break
 			}
 		}	
 	}
 })
-var main_lbt_7autoplay = setInterval(function(){
-	$(".game_next").click()
-},2000)
-$(".game_bg .left_lbt").mouseenter(function(){
-	clearInterval(main_lbt_7autoplay)
-})
-$(".game_bg .left_lbt").mouseleave(function(){
-	main_lbt_7autoplay = setInterval(function(){
-		$(".game_next").click()
-	},2000)
-})
+
+// 登录页cookies记录
+var set = {}
+window.onload = function(){
+	$(".cook").click(function(){
+		if ($("#seven")[0].checked) {
+			set.username = $(".name").val()
+			set.password = $(".pass").val()
+		}
+		setCookie(set,30)
+	})
+		$(".name").val(getCookie("userName"))
+		$(".pass").val(getCookie("passWord"))
+}
+
+// 首页第一个轮播图ajax
+function callbackfn(data){
+	$imgUrl = $(".main_lbt_1").eq(0).find(".left").find("a").find("img")
+	$gameDetails = $(".main_lbt_1").eq(0).find(".left").find("#details")
+	$imgMinUrl = $(".main_lbt_1").eq(0).find(".right").find(".min_pic").find("img")
+	$imgSpan = $(".main_lbt_1").eq(0).find(".left").find(".left_lbt")
+	$imgMinLbtUrl = $(".main_lbt_1").eq(0).find(".min_lbt").find(".lbt_pic").find("img")
+	$gameName = $(".Name").eq(0).find("a")
+	$MingameName = $(".main_lbt_1").eq(0).find(".min_lbt").find(".days_1")
+	$MingameDay = $(".main_lbt_1").eq(0).find(".min_lbt").find(".days_2")
+	$allpince = $(".main_lbt_1").eq(0).find(".min_lbt").find(".allpince")
+	$lableliList = $(".main_lbt_1").eq(0).find(".min_lbt").find(".list").find("li")
+	$zheKou = $(".main_lbt_1").eq(0).find(".right").find(".zhekou").find(".zhekounum")
+	$bottOld = $(".main_lbt_1").eq(0).find(".right").find(".bott").find(".bottold")
+	$bottNow = $(".main_lbt_1").eq(0).find(".right").find(".bott").find(".bottnow")
+	$win = $(".main_lbt_1").eq(0).find(".right").find(".bott").find(".win")
+		console.log(data)
+	for (var m = 0; m < data.length; m++) {
+		// var newmain = $(".main_lbt_1").clone(true)
+		// $(".body").appendTo(newmain)
+		Name = data[m].name
+		bgUrl = data[m].url
+		urlList = (data[m].imgUrl)
+		minDay = data[m].date
+		arr = minDay.split("-")
+		gameThink = data[m].evaluate
+		pince = data[m].evaluatingCount
+		lableList = data[m].label
+		zhekouOr = data[m].isSale
+		bottOldNum = data[m].originPrice
+		bottNowNum = data[m].price
+		zhekouhow = data[m].discount
+		Win = data[m].platform
+		for (var a = 0; a < urlList.length; a++) {
+			Url = urlList[a]
+			$gameName.html(Name)
+			$MingameName.html(Name)
+			$imgMinUrl.eq(a).attr("src",Url)
+			$imgMinLbtUrl.eq(a).attr("src",Url)
+			$imgUrl.eq(a).attr("src",Url)
+			$gameDetails.eq(a).attr("href",bgUrl)
+			$imgSpan[0].style.backgroundImage = "url" + "(" + urlList[0] + ")"
+			$MingameDay.eq(0).html("发行于：" + arr[0] + "年" + arr[1] + "月" + arr[2] + "日")
+			if (gameThink == 1) {
+				$allpince.html("好评如潮 " + "(" + pince + ")")
+			}
+			if (gameThink == 2) {
+				$allpince.html("特别好评 " + "(" + pince + ")")
+			}
+			if (gameThink == 3) {
+				$allpince.html("多半好评 " + "(" + pince + ")")
+			}
+			if (gameThink == 4) {
+				$allpince.html("褒贬不一 " + "(" + pince + ")")
+			}
+			if (gameThink == 5) {
+				$allpince.html("多半差评 " + "(" + pince + ")")
+			}
+			if (gameThink == 6) {
+				$allpince.html("差评如潮 " + "(" + pince + ")")
+			}
+			if (gameThink == 7) {
+				$allpince.html("无评论 " + "(" + pince + ")")
+			}
+			if (zhekouOr) {
+				$zheKou.html("-" + zhekouhow * 100 + "%")
+				$bottOld.html("￥" + bottOldNum)
+				$bottNow.html("￥" + bottNowNum)
+			}
+			// if (Win.length == 3) {
+			// 	$win[2].style.backgroundImage = "url" + "(" + "images/st.png" + ")"
+			// 	$win[1].style.backgroundImage = "url" + "(" + "images/ios.png" + ")"
+			// 	$win[0].style.backgroundImage = "url" + "(" + "images/win.png" + ")"
+			// }
+			// if (Win.length == 2) {
+			// 	if (Windows) {
+			// 		$win[0].style.backgroundImage = "url" + "(" + "images/win.png" + ")"	
+			// 	}
+			// 	if (MacOS) {
+			// 		$win[Win.length-1].style.backgroundImage = "url" + "(" + "images/ios.png" + ")"	
+			// 	}
+			// 	if (Steam) {
+			// 		$win[Win.length-1].style.backgroundImage = "url" + "(" + "images/st.png" + ")"	
+			// 	}
+			// }
+			// if (Win.length == 1) {
+			// 	if (Windows) {
+			// 		$win[0].style.backgroundImage = "url" + "(" + "images/win.png" + ")"	
+			// 	}
+			// 	if (MacOS) {
+			// 		$win[0].style.backgroundImage = "url" + "(" + "images/ios.png" + ")"	
+			// 	}
+			// 	if (Steam) {
+			// 		$win[0].style.backgroundImage = "url" + "(" + "images/st.png" + ")"	
+			// 	}
+			// }
+		}
+		for (var b = 0; b < 6; b++) {
+			$lableliList.eq(b).html(lableList[b])
+		}
+	}	
+}
+window.onload = function(){
+	var script = document.createElement("script")
+	script.setAttribute("src","http://www.qinsichina.com/steamDataAPI.php?callback=callbackfn")
+	document.getElementsByTagName("head")[0].appendChild(script)
+}
